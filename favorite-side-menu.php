9e376fdf360e4563d853f229a0105865e3295806
kaiyu li <krisfk@gmail.com>
2021-05-11 23:50:29 +0800

    .

--- favorite-side-menu.php
+++ favorite-side-menu.php
@@ -1,15 +1,17 @@
 <h1>好想買清單</h1>
-<ul class="no-dot">
+<ul class="no-dot favorite-side-menu-ul">
 
 
     <li> <a href="javascript:void(0)">
 
-
+            <img src="./images/heart.png" alt="">
             好想買清單</a></li>
     <li><a class="order-history-a" href="javascript:void(0)">
-
+            <img src="./images/fav-bought-icon.png" alt="">
             好想買過的產品</a></li>
     <li><a class="delivery-address-a" href="javascript:void(0)">
+            <img src="./images/fav-seem-icon.png" alt="">
+
             最近睇過的產品</a></li>
 
 </ul>
--- favorite.php
+++ favorite.php
@@ -11,59 +11,78 @@
 
 
             <div class="row">
-                <div class="col-lg-2 col-md-12 col-sm-12 col-12 profile-side-menu">
-                    <?php //include('profile-side-menu.php');?>
+                <div class="col-lg-2 col-md-12 col-sm-12 col-12 favorite-side-menu">
+                    <?php //include('favorite-side-menu.php');?>
                     <?php include('favorite-side-menu.php');?>
 
                 </div>
-                <div class="col-lg-10 col-md-12 col-sm-12 col-12 profile-content-div">
+                <div class="col-lg-10 col-md-12 col-sm-12 col-12 favorite-content-div ">
 
+
+                    <div class="row g-0  mt-2 favorite-title-div mb-3">
+                        <div class="col-6 favorite-title">
+                            <span class="fw-bold">好想買</span>清單
+                        </div>
+                        <div class="col-6 text-end select-all-unselect-all-div">
+
+                            <input class="form-check-input check-fav-option" type="checkbox" value=""
+                                id="check-fav-option-1">
+                            <label for="check-fav-option-1"> 全選</label>
+                            <input class="form-check-input check-fav-option" type="checkbox" value=""
+                                id="check-fav-option-2">
+                            <label for="check-fav-option-2">取消已選</label>
+
+                        </div>
+
+                    </div>
                     <ul class="product-list-ul product-list-ul-col5">
                         <?php for($i=0;$i<30;$i++)
             {?>
                         <li class="mb-5 product-li">
+
+
+
                             <div class="product-div-1">
+                                <input class="form-check-input" type="checkbox" value="" id="">
 
-                                <div class="text-center position-relative product-img-wrapper">
-
-                                    <!-- 
-                                    <div class="position-absolute w-100 h-100 hover-layer text-start">
-                                        <ul>
-                                            <li> <a href="./product-detail.php" class="hover-layer-btn"> <img
-                                                        class="eye-icon" src="./images/eye-icon.png" alt="">快速睇產品</a>
-                                            </li>
-
-                                            <li> <a href="" class="hover-layer-btn"><img class="heart-icon"
-                                                        src="./images/heart-icon.png" alt="">加入好想買清單</a></li>
-
-                                        </ul>
-
-                                    </div> -->
-                                    <img class="product-img w-100" src="./images/product-foto-1.jpg" alt="">
+                                <div class="float-end small-icon-div">
+                                    <a href="#" class="yellow-share-icon-a"> <img class="yellow-share-icon"
+                                            src="./images/yellow-share-icon.png" alt=""></a>
+                                    <a href="#" class="bin-btn-a"> <img class="bin-btn" src="./images/bin-btn.png"
+                                            alt=""></a>
 
                                 </div>
+                                <div class="mobile-table-div">
+                                    <div class="text-center position-relative product-img-wrapper">
 
 
 
-                                <div class="product-name">日版 多啦A夢 叮噹造型曲奇餅禮盒（1盒6塊）</div>
-                                <div class="product-set mt-1">1盒6塊</div>
+                                        <img class="product-img w-100" src="./images/product-foto-1.jpg" alt="">
 
-                                <div class="recommend-rate  mt-1">
-                                    好想買指數：
-
-                                    <div class="pt-div"><img class="pt" src="./images/green-pt.png" alt=""> <img
-                                            class="pt" src="./images/green-pt.png" alt=""> <img class="pt"
-                                            src="./images/green-pt.png" alt="">
-                                        <img class="pt" src="./images/green-pt.png" alt="">
-                                        <img class="pt" src="./images/grey-pt.png" alt="">
                                     </div>
 
-                                </div>
-                                <div class="limited-quantity  mt-1">日本限量版1000件</div>
-                                <div class="old-price price  mt-1">HK$79.9</div>
-                                <div class="final-price"><span class="s">$</span><span class="b">9999</span><span
-                                        class="s">.99</span></div>
+                                    <div class="product-info-right">
 
+                                        <div class="product-name">日版 多啦A夢 叮噹造型曲奇餅禮盒（1盒6塊）</div>
+                                        <div class="product-set mt-1">1盒6塊</div>
+
+                                        <div class="recommend-rate  mt-1">
+                                            好想買指數：
+
+                                            <div class="pt-div"><img class="pt" src="./images/green-pt.png" alt=""> <img
+                                                    class="pt" src="./images/green-pt.png" alt=""> <img class="pt"
+                                                    src="./images/green-pt.png" alt="">
+                                                <img class="pt" src="./images/green-pt.png" alt="">
+                                                <img class="pt" src="./images/grey-pt.png" alt="">
+                                            </div>
+
+                                        </div>
+                                        <div class="limited-quantity  mt-1">日本限量版1000件</div>
+                                        <div class="old-price price  mt-1">HK$79.9</div>
+                                        <div class="final-price"><span class="s">$</span><span
+                                                class="b">9999</span><span class="s">.99</span></div>
+                                    </div>
+                                </div>
                                 <a href="javascript:void(0)" class="cart-btn mt-3"> <img src="./images/cart-icon2.png"
                                         alt="">
                                     加入購物車</a>
@@ -108,12 +127,47 @@
 <script type="text/javascript">
 $(function() {
 
-    $('.account-a').addClass('active');
-    $('.gender-button').click(function() {
+    $('.check-fav-option').click(function() {
 
-        $('.gender-button').removeClass('active')
-        $(this).addClass('active');
+        var idx = $(this).index();
+
+
+        if ($(this).is(':checked')) {
+            $('.check-fav-option').prop('checked', false);
+            $(this).prop('checked', true);
+
+            if (idx == 0) {
+
+                $('.product-div-1 input[type="checkbox"]').prop('checked', true)
+
+            }
+            if (idx == 2) {
+
+                $('.product-div-1 input[type="checkbox"]').prop('checked', false)
+
+            }
+
+
+        } else {
+            // alert(50)
+            $(this).prop('checked', false);
+
+            // if (idx === 0) {
+
+            //     $('.product-div-1 input[type="checkbox"]').prop('checked', true)
+            // }
+            // if (idx === 2) {
+            $('.product-div-1 input[type="checkbox"]').prop('checked', false)
+
+            // }
+
+        }
+
+
+
+
     })
+
 })
 </script>
 <?php include('footer.php')?>
